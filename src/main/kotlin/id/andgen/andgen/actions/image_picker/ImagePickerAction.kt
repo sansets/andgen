942fa24e101745bac4fdcf5e9452c1df86638313
Sandi Setiawan <sandisetiawan444@gmail.com>
2023-01-13 01:31:36 +0700

    add image picker top menu action

---
+++ src/main/kotlin/id/andgen/andgen/actions/image_picker/ImagePickerAction.kt
@@ -0,0 +1,11 @@
+package id.andgen.andgen.actions.image_picker
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ImagePickerAction : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+
+    }
+}
